4927f9158a73d369694a583773d0c26185634863
surajthite <suraj.thite@colorado.edu>
2020-09-28 23:52:01 -0600

    Commented COde !!

--- Lab1/SRC/main.cpp
+++ Lab1/SRC/main.cpp
@@ -35,15 +35,16 @@ Description : This file contains the Application code for lab0 exercisedsfsd
 int verbose_flag;
 using namespace std;
 
-
+/* Function Declaration for Parellel Merge and Bucket Sort*/
 int get_bucket_range(vector<int>& arr, int size, int thread);
 void *bucketSort(void *arg);
-void *merge_sort123(void *arg);
+void *fj_merge(void *arg);
 
-pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_barrier_t barrier;
-vector <multiset <int32_t>> Bucket;
-struct timespec start_time, end_time;
+pthread_mutex_t bucket_lock = PTHREAD_MUTEX_INITIALIZER; //Static Lock Initialization
+pthread_barrier_t barrier;// Barrier for Synchronization
+
+vector <multiset <int32_t>> Bucket; //Global Bucket to store values
+struct timespec start_time, end_time; //Struct to store the timer values
 
 int main (int argc, char **argv)
 {
@@ -51,7 +52,7 @@ int c = 0;
 char *buff = NULL;
 size_t temp =0;
 int count =0;
-FILE *fptr = NULL;
+FILE *fptr = NULL; //Pointer to access the file
 
 struct handler handler_t = {"Suraj Bajrang Thite"};
 if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of arguments
@@ -66,7 +67,7 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
           {"name", no_argument, &verbose_flag, 'n'}, //Long option ofr --name
           {"alg",  required_argument, 0, 'a'},  //long option for alg = merge/quick
           {"out",  required_argument,  0, 'o'}, //option for output file
-          {"thread",    required_argument, 0, 't'}, //option for input file
+          {"thread",    required_argument, 0, 't'}, //option for threads to be implemented
           {0, 0, 0,0}
         };
       int option_index = 0;
@@ -89,7 +90,7 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
         if(strcmp(optarg,"fjmerge")==0)
           handler_t.merge_sort = 1; //Set the merge sort flag
         if(strcmp(optarg,"lkbucket")==0)
-          handler_t.bucket = 1; //Set the quick sort flag
+          handler_t.bucket = 1; //Set the bucket flag
         break;
 
        case 'o':
@@ -101,16 +102,14 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
           break;
         case 't':
           //  printf("The number of threads passed is %d",atoi(optarg));
-          handler_t.thread_cnt =  atoi(optarg);
+          handler_t.thread_cnt =  atoi(optarg); //store the thread count in the variable
           printf("The number of threads passed is %d",  handler_t.thread_cnt);
 
         case '?':
-          // printf("Error!"); //Error options
           break;
 
         default:
-             printf("Incorrect no of Arguments Passed \nUse format ./mysort [--name] [sourcefile.txt] [-o outputfile.txt] [--alg=<fjmerge, lkbucket>] \n"); //Print error message
-
+        printf("Incorrect no of Arguments Passed \nUse format ./mysort [--name] [sourcefile.txt] [-o outputfile.txt] [-t <no of threads>][--alg=<fjmerge,lkbucket>] \n");
         }
     }
   for(; optind < argc; optind++){
@@ -123,14 +122,15 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
     exit (0);
   }
 
-  if (handler_t.thread_cnt <= 0) {
-		handler_t.thread_cnt = 1;
+  if (handler_t.thread_cnt <= 0)
+  {
+		handler_t.thread_cnt = 1; //Set thread count to one if invalid thread count is passed
 	}
 
-  fptr = fopen(handler_t.input_file,"r");
+  fptr = fopen(handler_t.input_file,"r");  //Open the file in read mode
   if(fptr == NULL)
   {
-    printf("\nError in opening the input text file. Please check again !\n");
+    printf("\nError in opening the input text file. Please check again !\n"); //Throw an exception incase of failure
     return -1;
   }
   while(getline(&buff,&temp,fptr) != -1)
@@ -140,10 +140,7 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
   handler_t.f_size = count;
   //printf("\nhandler_t.f_size = %d",  handler_t.f_size);
   int array_size = ((handler_t.f_size)); //Store the number of elements in varibale in the handler structure
-  vector<int> list(array_size);
-
-  pthread_t threads[handler_t.thread_cnt];
-  //int list[array_size]; //Array to store the data from input file
+  vector<int> list(array_size); //vector to store the numbers
   if(file_to_array(handler_t,list)!=0)  //Store data from file to array
   {
     printf("\nCannot write to file"); //Print in case of bad FD
@@ -151,60 +148,69 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
   }
 
   int list_array[list.size()];
-  std::copy(list.begin(), list.end(), list_array);
+  std::copy(list.begin(), list.end(), list_array); //Convert the vector to array
+
   printf("\n************** The Input array is *****************\n");
     for(int i=0;i<handler_t.f_size;i++)
     {
       printf("%d\n",list[i]); //Print the input read from the file
     }
 
+    pthread_t threads[handler_t.thread_cnt];
+
     if(handler_t.merge_sort)
       {
+        int i = 0, low = 0 ,len =0;
+        pthread_barrier_init(&barrier, NULL, handler_t.thread_cnt); //Initialize the barrier
         printf("\n Executing Merge Sort !! ");
-        struct task *merge_task;
-        struct task merge_task_list[handler_t.thread_cnt];
-        int len = (array_size / handler_t.thread_cnt);
-        int i = 0, low = 0;
-        pthread_barrier_init(&barrier, NULL, handler_t.thread_cnt);
-        for (i = 0; i < handler_t.thread_cnt; i++, low += len) {
 
-          merge_task = &merge_task_list[i];
-          merge_task->task_no = i;
-          merge_task->list = list_array;
+        struct merge_task *merge_task_args;  // pointer to structure arguments
 
-          merge_task->task_low = low;
-          merge_task->task_high = low + len - 1;
+        struct merge_task merge_args_list[handler_t.thread_cnt]; // list to store arguments to be passed to threads
 
-          if (i == (handler_t.thread_cnt - 1)) {
-            merge_task->task_high = array_size - 1;
-          }
+        len = (array_size / handler_t.thread_cnt); // calculate the length of the elements to be passed into each thread
+
+        for (i = 0; i < handler_t.thread_cnt; i++, low += len)
+        {
+          merge_task_args = &merge_args_list[i]; // point to arguments of ith thread
+
+          merge_task_args->task_no = i; // Store thread id
+          merge_task_args->task_low = low; // store the lower index to be sorted
+          merge_task_args->task_high = low + len - 1; //store the higher index of the elements to be sorted
+          merge_task_args->list = list_array; //pass the array to the thread
+
+          if (i == (handler_t.thread_cnt - 1))
+            merge_task_args->task_high = array_size - 1; // size = arraysize -1 for the last thread
         }
 
         for (i = 0; i < handler_t.thread_cnt; i++) {
-          merge_task = &merge_task_list[i];
-          if ((pthread_create(&threads[i], NULL, merge_sort123, merge_task)) != 0) {
-            printf("Error on creating the thread\n");
-                    exit(1);
-          } else {
-            printf("Creating thread %d\n", (i+1));
+          merge_task_args = &merge_args_list[i];
+          if ((pthread_create(&threads[i], NULL, fj_merge, merge_task_args)) != 0) // Create the threads and pass the arguments
+          {
+            printf("Error:  Couldn't create threads\n");
+            exit(1);
+          } else
+          {
+            printf("Thread %d started ....\n", (i+1));
           }
         }
 
-            // joining all 4 threads
-        for (i = 0; i < handler_t.thread_cnt; i++) {
-          printf("Joining thread %d\n", (i+1));
-          pthread_join(threads[i], NULL);
+        for (i = 0; i < handler_t.thread_cnt; i++)
+         {
+           pthread_join(threads[i], NULL); // join the threads to the main thread
+           printf("Thread %d Joined ...\n", (i+1));
         }
 
-        struct task *task_0 = &merge_task_list[0];
-        for (i = 1; i < handler_t.thread_cnt; i++) {
-          struct task *tsk = &merge_task_list[i];
+        struct merge_task *task_0 = &merge_args_list[0]; // select the thread 0 arguments
+
+        for (i = 1; i < handler_t.thread_cnt; i++)
+        {
+          struct merge_task *tsk = &merge_args_list[i]; // merge the arguments in the list
           merge(tsk->list, task_0->task_low, tsk->task_low - 1, tsk->task_high);
         }
-
-        clock_gettime(CLOCK_MONOTONIC, &end_time);
-        pthread_barrier_destroy(&barrier);
-        printf("\nWriting Data to the file");
+        clock_gettime(CLOCK_MONOTONIC, &end_time); // store the end time
+        pthread_barrier_destroy(&barrier); //destroy the barrier since its not is use in any more
+        printf("\nWriting Data to the file"); //Write the data to the file
         printf("\n************** The Sorted array is ***************** \n");
           for(int i=0;i<handler_t.f_size;i++)
           {
@@ -216,26 +222,25 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
     {
       printf("Executing Bucket Sort"); //Execute quick algorithm if --al=quick is passed as argument
       //quickSort(list, 0, (array_size - 1));
-      int i = 0, k = 0;
+      int i = 0, k = 0, m = 0;
       struct bucket_task b_task[handler_t.thread_cnt];
-      int divider = get_bucket_range(list, array_size, handler_t.thread_cnt);
-      Bucket.resize(handler_t.thread_cnt);
-      int len = (array_size / handler_t.thread_cnt);
-      int m = 0;
+      int divider = get_bucket_range(list, array_size, handler_t.thread_cnt); // get the divider
+      Bucket.resize(handler_t.thread_cnt); // resize the bucket
+      int len = (array_size / handler_t.thread_cnt); // determine the number of elements to be passed to each thread
 
-      pthread_barrier_init(&barrier, NULL, handler_t.thread_cnt);
+      pthread_barrier_init(&barrier, NULL, handler_t.thread_cnt); //Initialize the barrier
 
       for (i = 0; i < handler_t.thread_cnt; i++)
       {
         m = i*len;
-        if (i != (handler_t.thread_cnt - 1))
+        if (i != (handler_t.thread_cnt - 1)) //set arguments for last thread
         {
           b_task[i].t_divider = divider;
           b_task[i].t_id = i;
           b_task[i].t_size = len;
           b_task[i].list = &list[m];
         }
-        else
+        else  //Set arguments for the remaining threads
         {
           b_task[i].t_divider = divider;
           b_task[i].t_id = i;
@@ -243,7 +248,7 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
           b_task[i].list = &list[m];
         }
 
-        if ((pthread_create(&threads[i], NULL, bucketSort, (void *)&b_task[i])) != 0)
+        if ((pthread_create(&threads[i], NULL, bucketSort, (void *)&b_task[i])) != 0) // Create threads and pass the arguments
         {
           printf("Error on creating the thread\n");
           exit(1);
@@ -257,19 +262,19 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
       for (i = 0; i < handler_t.thread_cnt; i++)
       {
         printf("Joining thread %d\n", (i+1));
-        pthread_join(threads[i], NULL);
+        pthread_join(threads[i], NULL); // Join the threads
       }
       int vec_len = (int)Bucket.size();
       for (i = 0; i < vec_len; i++) {
-      for (std::multiset<int>::iterator j = Bucket[i].begin(); j != Bucket[i].end(); ++j)
+      for (std::multiset<int>::iterator j = Bucket[i].begin(); j != Bucket[i].end(); j++)
       {
         list[k] = *j;
         k++;
       }
       }
-      clock_gettime(CLOCK_MONOTONIC,&end_time);
+      clock_gettime(CLOCK_MONOTONIC,&end_time); // determine the end time
       pthread_barrier_destroy(&barrier);
-      printf("\nWriting Data to the file");
+      printf("\nWriting Data to the file"); //Write the data to file
       printf("\n************** The Sorted array is ***************** \n");
         for(int i=0;i<handler_t.f_size;i++)
         {
@@ -278,20 +283,20 @@ if ((argc < 2) || (argc > 7) || (argc < 7 && argc >2)) // Check for number of ar
     array_to_file(handler_t, list); // Store the sorted data to a ouput file passed as argument
     }
 
-
 unsigned long long elapsed_ns;
-elapsed_ns = (end_time.tv_sec-start_time.tv_sec)*1000000000 + (end_time.tv_nsec-start_time.tv_nsec);
+elapsed_ns = (end_time.tv_sec-start_time.tv_sec)*1000000000 + (end_time.tv_nsec-start_time.tv_nsec); // calculate the time required for completion of the thread
 double elapsed_s = ((double)elapsed_ns)/1000000000.0;
-printf("Elapsed (s): (ns) %lf : %llu \n",elapsed_s,elapsed_ns);
+printf("Elapsed (s): (ns) %lf : %llu \n",elapsed_s,elapsed_ns); //Print the time required to sort the array by n thread
 exit (0);
 }
 
 /*
 Name : file_to_array
 Description : Function to read the unsorted data from file to an array
-INput: handler and array where the data is to be stored from the file
+INput: handler and array where the data is to be stored from the file and vector where the data to be stored
 Return: -1 if error in opening a file , 0 if data has been sucessfully copied from file to an array
 */
+
 int file_to_array(struct handler handler_t, vector<int>& buffer)
 {
   FILE *file_pointer;
@@ -312,7 +317,7 @@ int file_to_array(struct handler handler_t, vector<int>& buffer)
 /*
 Name : array_to_file
 Description : Function to write the sorted data to the file*
-INput: handler and array where the data is to be written to the file
+INput: handler and vector  where the data is to be written to the file
 Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
 */
 
@@ -333,6 +338,13 @@ int array_to_file(struct handler handler_t, vector<int>& buffer)
   return 0;
 }
 
+/*
+Name : array_to_file
+Description : Function to write the sorted data to the file*
+INput: handler and array where the data is to be written to the file
+Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
+*/
+
 int array_to_file(struct handler handler_t, int buffer[])
 {
   FILE *file_pointer;
@@ -349,11 +361,48 @@ int array_to_file(struct handler handler_t, int buffer[])
   fclose(file_pointer); //Clovector<int> listse the FD
   return 0;
 }
+
+
 /*
-Name : array_to_file
-Description : Function to write the sorted data to the file*
-INput: handler and array where the data is to be written to the file
-Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
+Name : void *fj_merge(void *arg)
+Description : This function implements parellel merge sort to be executed in threads in execution using fork join
+INput: merge_task arguments
+Return: void
+*/
+
+void *fj_merge(void *arg)
+{
+  int low =0,high =0 , mid=0;
+	struct merge_task *merge_args = (struct merge_task *)arg;
+
+	pthread_barrier_wait(&barrier);
+
+	if(merge_args->task_no == 0){
+		clock_gettime(CLOCK_MONOTONIC,&start_time);
+	}
+
+	pthread_barrier_wait(&barrier);
+
+	printf("Executing thread %d\n", (merge_args->task_no + 1));
+
+  low = merge_args->task_low;
+  high = merge_args->task_high;
+  mid = low + (high - low) / 2;
+
+  if (low < high)
+  {
+      mergesort(merge_args->list, low, mid);
+      mergesort(merge_args->list, mid + 1, high);
+      merge(merge_args->list, low, mid, high);
+    }
+    return 0;
+}
+
+/*
+Name : int get_bucket_range(vector<int>& arr, int size, int thread)
+Description : This fucntion detemines the divider depending upon the maximum value in the array
+Input: Input array to be sorted, size and number of thread
+Return:  divider
 */
 
 int get_bucket_range(vector<int>& arr, int size, int thread)
@@ -371,10 +420,10 @@ int get_bucket_range(vector<int>& arr, int size, int thread)
 }
 
 /*
-Name : array_to_file
-Description : Function to write the sorted data to the file*
-INput: handler and array where the data is to be written to the file
-Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
+Name : void *bucketSort(void *arg)
+Description : This function is executed in the thread using Bucket Sort.
+Input: bucket_task structure argument.
+Return: void
 */
 
 void *bucketSort(void *arg)
@@ -391,39 +440,10 @@ void *bucketSort(void *arg)
 	for (i = 0; i < b_task->t_size; i++)
   {
 		j=floor( b_task->list[i] / b_task->t_divider );
-		pthread_mutex_lock(&lock1);
+		pthread_mutex_lock(&bucket_lock);
 		Bucket[j].insert((b_task->list)[i]);
-		pthread_mutex_unlock(&lock1);
+		pthread_mutex_unlock(&bucket_lock);
 	}
 	//pthread_barrier_wait(&barrier);
 	return 0;
-}
-
-void *merge_sort123(void *arg)
-{
-  int low =0,high =0 , mid=0;
-	struct task *merge_args = (struct task *)arg;
-
-	pthread_barrier_wait(&barrier);
-
-	if(merge_args->task_no == 0){
-		clock_gettime(CLOCK_MONOTONIC,&start_time);
-	}
-
-	pthread_barrier_wait(&barrier);
-
-	printf("Executing thread %d\n", (merge_args->task_no + 1));
-
-  low = merge_args->task_low;
-  high = merge_args->task_high;
-
-    int mid = low + (high - low) / 2;
-
-    if (low < high) {
-        mergesort(merge_args->list, low, mid);
-        mergesort(merge_args->list, mid + 1, high);
-        merge(merge_args->list, low, mid, high);
-    }
-
-    return 0;
 }
--- Lab1/SRC/main.h
+++ Lab1/SRC/main.h
@@ -16,6 +16,8 @@
 #include <algorithm>
 #include <set>
 #include <iterator>
+
+
 /* Structure to store the input arguments from the terminal */
 
 struct handler
@@ -24,27 +26,29 @@ struct handler
   char *input_file; //Pointer for input file name
   int f_size; // Size of input file
   char *output_file; // Pointer to output file name.
-  int merge_sort;
-  int bucket;
-  int thread_cnt;
-  bool algo; //0  for Merge Sort and 0 for Buck Sort Implementation
+  int merge_sort; //Flag to determine whether the sort is merge sort
+  int bucket; //Flag to determine whehter bucket sort is to be implemented
+  int thread_cnt; //Variable  to store the thread count
+//  bool algo; //0  for Merge Sort and 1 for Buck Sort Implementation
 };
 
-
+/* Structure to store the data set for each task */
 struct bucket_task
  {
-  int t_id;
-  int t_divider;
-  int t_size;
-  int *list;
+  int t_id; //varibale to store thread id associated with the bucket
+  int t_divider; //Varaible to store the divider task
+  int t_size; //varibale to store the size of bucket indicated by the thread
+  int *list; //pointer to the starting element of list
 };
 
-struct task {
-	int task_no;
-	int task_low;
-	int task_high;
-	int *list;
+/*structure to store the data set for merge thread*/
+struct merge_task {
+	int task_no; //varibale to store thread id associated with the merge thread
+	int task_low; //Varibale to store  the lowest index of the data set to be sorted by the thread
+	int task_high;//Varibale to store the highest index of the data set to be sorted by the thread
+	int *list; //pointer to the starting element of list
 };
+
 /*
 Name : file_to_array
 Description : Function to read the unsorted data from file to an array
@@ -56,12 +60,21 @@ int file_to_array(struct handler handler_t, std::vector<int>& buffer);
 
 /*
 Name : array_to_file
-Description : Function to write the sorted data to the file*
+Description : Function to write the sorted data to the file* using input vector 
 INput: handler and array where the data is to be written to the file
 Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
 */
 
 int array_to_file(struct handler handler_t,  std::vector<int>& buffer);
+
+/*
+Name : array_to_file
+Description : Over Riding Function to write the sorted data to the file* using input array
+INput: handler and array where the data is to be written to the file
+Return: -1 if error in opening a file , 0 if data has been sucessfully written from array to the specified file
+*/
+
+
 int array_to_file(struct handler handler_t,  int buffer[]);
 
 
